e0913598da280103f2f656483a147cc463134e46
sophienocode <sophieprotanki@gmail.com>
2026-02-12 09:22:33 -0800

    (Auth): add ... smth new

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -8,6 +8,7 @@ export default function Dashboard() {
   return (
     <>
       <h1>Dashboard the website!</h1>
+      <h2>think in some ideas</h2>
 
 
 
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import { words } from "../constants";
 import Button from "../components/Button";
 
