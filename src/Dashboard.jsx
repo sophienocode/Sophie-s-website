3940a79e2fb9e09667415135ad7fca1c24ea0a5a
sophienocode <sophieprotanki@gmail.com>
2026-02-25 13:10:11 -0800

    (Auth): casual update

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -13,82 +13,15 @@ export default function Dashboard() {
        <h4>Ola ola oal ola ola ola</h4>
        <h1>helo world</h1>
        <h1>Dashboard the website!</h1>
-      
-       <h3>maybe a calendar?</h3>
+        <h3>maybe a calendar?</h3>
        <h4>Ola ola oal ola ola ola</h4>
        <h1>helo world</h1>
-
-       <h4>ola ola ola </h4>
-
        <h1>Dashboard the website!</h1>
-      
-       <h3>maybe a calendar?</h3>
-       <h4>Ola ola oal ola ola ola</h4>
-       <h1>helo world</h1>
-
-       <h4>ola ola ola </h4>
-       <h1>Dashboard the website!</h1>
-      
-       <h3>maybe a calendar?</h3>
-       <h4>Ola ola oal ola ola ola</h4>
-       <h1>helo world</h1>
-
-       <h4>ola ola ola </h4>
-       <h4>ola ola ola </h4>
-
-
-       <h3>maybe a calendar?</h3>
+        <h3>maybe a calendar?</h3>
        <h4>Ola ola oal ola ola ola</h4>
        <h1>helo world</h1>
        <h1>Dashboard the website!</h1>
       
-       <h3>maybe a calendar?</h3>
-       <h4>Ola ola oal ola ola ola</h4>
-       <h1>helo world</h1>
-
-       <h4>ola ola ola </h4>
-
-       <h1>Dashboard the website!</h1>
-      
-       <h3>maybe a calendar?</h3>
-       <h4>Ola ola oal ola ola ola</h4>
-       <h1>helo world</h1>
-
-       <h4>ola ola ola </h4>
-       <h1>Dashboard the website!</h1>
-      
-       <h3>maybe a calendar?</h3>
-       <h4>Ola ola oal ola ola ola</h4>
-       <h1>helo world</h1>
-
-       <h4>ola ola ola </h4>
-       <h4>ola ola ola </h4><h3>maybe a calendar?</h3>
-       <h4>Ola ola oal ola ola ola</h4>
-       <h1>helo world</h1>
-       <h1>Dashboard the website!</h1>
-      
-       <h3>maybe a calendar?</h3>
-       <h4>Ola ola oal ola ola ola</h4>
-       <h1>helo world</h1>
-
-       <h4>ola ola ola </h4>
-
-       <h1>Dashboard the website!</h1>
-      
-       <h3>maybe a calendar?</h3>
-       <h4>Ola ola oal ola ola ola</h4>
-       <h1>helo world</h1>
-
-       <h4>ola ola ola </h4>
-       <h1>Dashboard the website!</h1>
-      
-       <h3>maybe a calendar?</h3>
-       <h4>Ola ola oal ola ola ola</h4>
-       <h1>helo world</h1>
-
-       <h4>ola ola ola </h4>
-       <h4>ola ola ola </h4>
-  
   
       
 
